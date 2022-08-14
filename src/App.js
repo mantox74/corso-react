d2496c10c684807ff8131e626043e4e62f864db1
mantox74 <mantovani.massimiliano@gmcil.com>
2022-08-14 20:54:40 +0200

    prova SSH

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Prima app con React
+          Prima app con React test
         </p>
         <a
           className="App-link"
